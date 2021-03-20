2eca626e71c3f3fe4d3551906277bbe596ac66d7
Miguel <miguel.diaz5@catolica.edu.sv>
2021-03-20 00:04:45 -0600

    comit

---
+++ ConsoleApp1/Module1.vb
@@ -0,0 +1,23 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim numeros(3) As Integer
+        Dim suma As Integer = 0
+
+        For i = 0 To numeros.Length - 1 Step 1
+            Console.WriteLine("ingrese un valor: " & i + 1)
+            numeros(i) = Console.ReadLine()
+            suma += numeros(i)
+        Next
+
+        Array.Sort(numeros)
+
+        Console.WriteLine("suma total: " & suma)
+        Console.ReadLine()
+        Console.WriteLine(numeros)
+
+
+    End Sub
+
+End Module
+
